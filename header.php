de13e3513104d0f3339d00cb863de8a1c8ea6697
daanberg <6013014@mborijnland.nl>
2022-02-21 11:13:18 +0100

    Header Begin

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Daan Berg">
+    <meta name="keywords" content="">
+    <title>Header</title>
+
+    <link rel="stylesheet" type="text/css" href="CSS/common.css"> 
+
+</head>
+<header id="nav-bar">
+<img src="img/logo.png" alt="Logo">
+        <nav>
+        <section id="navbar">
+            <ul>
+                <li><a href="#">C</a></li>
+                <li><a href="#">C</a></li>
+                <li><a href="#">C</a></li>
+                <li><a href="#">C</a></li>
+            </ul>
+            </section>
+            <section id="navbar-small">
+            <details>
+                <summary>
+                    <h1>☰</h1>
+                </summary>
+                <nav id="item-nav">
+                    <ul>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                    </nav>
+            </details>
+            </section>
+        </nav>
+    </header>
+</html>
