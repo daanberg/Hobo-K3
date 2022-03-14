08b3f70fe9a9e9fbdd31d7b1ad32389ea55abade
Ruurd <74601593+Lux42069@users.noreply.github.com>
2022-03-14 09:58:00 +0100

    Made account pagine

---
+++ Account.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title>Account</title>
+</head>
+<body>
+    <button type="button">profiel bewerken</button> 
+    <img src="./img/DaanPFP.jpg">
+</body>
+</html>
