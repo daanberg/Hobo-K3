e72fa3573e330d36fdf39a9feb58d8961f924830
Kevinxue12 <74967898+Kevinxue12@users.noreply.github.com>
2022-04-03 12:40:07 +0200

    allemaal nieuwe tech

--- test.php
+++ test.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <link rel="stylesheet" href="css/history.css">
     <title>Document</title>
+
+    <link rel="stylesheet" href="css/history.css">
+
 </head>
 <body>
     
     <!-- Header-->
 
     <header>
+                
             <nav>
-                <img src="img/hobologo.png" alt="" width="250px" height="70px">
+                <a href="#"><img src="img/logo2.png" alt="logo2.png" width="150px" height="60px"></a>
                 <a href="#">Home</a>
                 <a href="#">History</a>
                 <a href="#">New & popular</a>
@@ -29,26 +32,37 @@
 
         ?>
 
+        <?php
+              $conn = new mysqli($host, $user, $pass, $database); 
+              $sql = "SELECT SerieTitel FROM serie WHERE SerieId = 5;"; 
+              
+              $result = $conn->query($sql); 
+              
+              if($result){
+                 while ($row = $result->fetch_object()){
+                 echo$row->SerieTitel;
+             }
+              $result->close();
+              $conn->close();
+              }
+         ?>
         <h1>Recommendations</h1>
 
-    <section id="main">
+        <section class="row-home">
         <?php
             $conn = new mysqli($host, $user, $pass, $database); 
-             $sql = "SELECT SerieID FROM serie LIMIT 5;";  //SQL code hierin 
+             $sql = "SELECT SerieID FROM serie LIMIT 5;"; 
              
              $result = $conn->query($sql); 
              
              if($result){
                 foreach($result->fetch_all() as $row){
-                    // echo $row[0]."<br>";
-                    // var_dump($row);
                     echo "<img src='img/0000".$row[0].".jpg' >";
             }
              $result->close();
              $conn->close();
              }
         ?>
-
     </section>
     <h1>Zojuist afgekeken</h1>
 
