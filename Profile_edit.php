00cb6eed454eb889d712c78968f6cf6faaf0ed11
Ruurd <74601593+Lux42069@users.noreply.github.com>
2022-04-13 20:06:25 +0200

    changed small thing of pf edit page

--- Profile_edit.php
+++ Profile_edit.php
@@ -4,10 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./css/Style_Editpage">
+    <link rel="stylesheet" type="text/css" href="./css/style_prof.css">
     <title>Edit Profile</title>
 </head>
 <body>
-    <p>Hello</p>
+    <img class="logo"  src="./img/logo.png">
+
+    <div id="toprow">    
+        <input type="checkbox" name="kid" value="kid">
+        <label for="kid">Kid?</label><br>
+    </div>
+
+    <br><br>
+
+    <form id="bottomrow" action="/action_page.php">
+        <label for="pfname"></label>
+        <input type="text" id="pfname" name="pfname">
+        <input type="submit" value="Submit">
+    </form>
+
+<!-- Mobile -->
+
+<!-- Tablet and Big screen -->
+
 </body>
 </html>
