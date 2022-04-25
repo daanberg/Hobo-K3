c63fb5dda1a5f2c2878c229b54a1d80cc6b012f6
daanberg <6013014@mborijnland.nl>
2022-04-25 13:21:15 +0200

    Banners toegevoegd

--- class/user.php
+++ class/user.php
@@ -16,6 +16,67 @@ require_once 'DbConfig.php';
     }
 
 
+        public function create($data){
+            try{
+                if($data['password'] != $data['conf-password']){
+                    throw new Exception("Wachtwoorden zijn niet gelijk");
+                }
+                $passwordEncr = password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 12]);
+                $sql = "INSERT INTO klant (username, password) VALUE (:username, :password)";
+                $stmt = $this->connect()->prepare($sql);
+                $stmt->bindParam(":username", $data['username']);
+                $stmt->bindParam(":password", $passwordEncr);
 
-}
+                if($stmt->execute()){
+                    header("location: ./inlogpagina.php");
+                }
+            }
+            catch(Exception $e){
+                echo $e->getMessage();
+            }
+        }
+
+        public function getUser($username){
+            $sql = "SELECT username, password FROM klant WHERE username = :username";
+            $stmt= $this->connect()->prepare($sql);
+            $stmt->bindParam(":username", $username);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_OBJ);
+        }
+
+        public function login($data){
+            try{
+                $user = $this->getUser($data['username']);
+                if(!$user){
+                    throw new Exception("gebruiker bestaat niet.");
+                }
+                if(!password_verify($data['password'], $user->password)){
+                    throw new Exception("wachtwoord is incorrect.");
+                }
+                session_start();
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $user->username;
+                    
+                header("location: Home.php");
+
+            }catch(Exception $e){
+                echo $e->getMessage();
+            }
+        }
+
+        public function getPic1(){
+            $sql = "SELECT serieID FROM serie  WHERE serieID LIKE '21%' AND Actief = 1 LIMIT 5;";
+            $stmt= $this->connect()->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        public function getPic(){
+            $sql = "SELECT serieID FROM serie WHERE Actief = 1 LIMIT 5;";
+            $stmt= $this->connect()->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+    }
+
 ?>
