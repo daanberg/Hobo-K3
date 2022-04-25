7513b26192b2ab8508f7be419a2ab8d873556d23
daanberg <6013014@mborijnland.nl>
2022-04-25 21:37:53 +0200

    Inloggen met Admin en content manager werkend

--- class/user.php
+++ class/user.php
@@ -46,19 +46,36 @@ require_once 'DbConfig.php';
 
         public function login($data){
             try{
+                
                 $user = $this->getUser($data['username']);
-                if(!$user){
-                    throw new Exception("gebruiker bestaat niet.");
+                $pass = $this->getUser($passwordEncr);
+
+                switch ($user->username){
+                    case "Admin":
+                        if(!password_verify($data['password'], $user->password)){
+                            throw new Exception("wachtwoord is incorrect.");
+                        }else{header("Location: ./admin/admin.php");}
+                        break;
+                    case "Content_Manager":
+                        if(!password_verify($data['password'], $user->password)){
+                            throw new Exception("wachtwoord is incorrect.");
+                        }else{header("Location: ./admin/cm.php");}
+                        break;
+                    default:
+                    if(!password_verify($data['password'], $user->password)){
+                        throw new Exception("wachtwoord is incorrect.");
+                    }else{header("Location: Home.php");}
                 }
-                if(!password_verify($data['password'], $user->password)){
-                    throw new Exception("wachtwoord is incorrect.");
-                }
+                // if(!$user){
+                //     throw new Exception("gebruiker bestaat niet.");
+                // }
+                // if(!password_verify($data['password'], $user->password)){
+                //     throw new Exception("wachtwoord is incorrect.");
+                // }
                 session_start();
                 $_SESSION['loggedin'] = true;
                 $_SESSION['username'] = $user->username;
                     
-                header("location: Home.php");
-
             }catch(Exception $e){
                 echo $e->getMessage();
             }
