63e7b3a96490afc5613b52338a379765fdc2a837
Ruurd <74601593+Lux42069@users.noreply.github.com>
2022-04-28 15:00:49 +0200

    small edit

--- searchpagina.php
+++ searchpagina.php
@@ -5,15 +5,13 @@
     
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./css/searchpage.php">
+    <link rel="stylesheet" type="text/css" href="./css/searchpage.css">
     <title>search</title>
 </head>
 <body>
